e76d6d4446707ca6d8831a6dcd1340a3dd1c650f
Artyom <yom@artyom.me>
2016-08-11 04:18:43 +0200

    Something more complicated + inspector

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 
@@ -9,17 +10,20 @@ module Main
 where
 
 
-import BasePrelude hiding (on)
+import BasePrelude hiding (on, Control)
 -- Monads
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Class
 -- Text
 import qualified Data.Text.All as T
+import Data.Text.All (Text)
 -- Random
 import System.Random
 -- GTK
 import Graphics.UI.Gtk
 import Graphics.UI.Gtk.WebKit.WebView
+import Graphics.UI.Gtk.WebKit.WebSettings
+import Graphics.UI.Gtk.WebKit.WebInspector
 
 
 
@@ -35,16 +39,39 @@ main = do
   window `after` objectDestroy $
     mainQuit
 
-  -- Create a WebView inside.
-  webView <- webViewNew
-  set window [containerChild := webView]
+  v   <- vBoxNew False 2
+  bar <- entryNew
+  sw  <- scrolledWindowNew Nothing Nothing
+  wv  <- webViewNew
+  set window [
+    containerChild := v,
+    containerBorderWidth := 2 ]
+  wvs <- get wv webViewWebSettings
+  set wvs [
+    webSettingsEnableDeveloperExtras := True ]
+  wvi <- get wv webViewInspector
+  wvi `on` inspectWebView $ \_ -> do
+    iw <- windowNew
+    iwv <- webViewNew
+    set iw [
+      containerChild := iwv ]
+    widgetShowAll iw
+    return iwv
 
-  -- Make the WebView display a random number on key press.
-  webView `on` keyReleaseEvent $ lift $ do
-    x <- randomIO :: IO Int
-    webViewLoadString webView (show x) Nothing ""
-    print x
-    return True
+  boxPackStart v bar PackNatural 0
+  boxPackStart v sw PackGrow 0
+  set sw [
+    containerChild := wv ]
+
+  bar `on` entryActivate $ do
+    url :: Text <- entryGetText bar
+    webViewLoadUri wv url
+
+  --wv `on` keyReleaseEvent $ tryEvent $ do
+  --  [Control] <- eventModifier
+  --  "i" <- eventKeyName
+  --  webViewInspector
+
 
   -- Run GTK.
   widgetShowAll window
